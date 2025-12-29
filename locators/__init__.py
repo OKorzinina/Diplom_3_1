5e673c12de7508a189cfadf8996d8542ede4bf36
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-29 20:54:52 +0100

    Update __init__.py

--- locators/__init__.py
+++ locators/__init__.py
@@ -1,1 +1,5 @@
+from .auth_page_locators import AuthPageLocators
+from .main_page_locators import MainPageLocators
+from .order_page_locators import OrderPageLocators
 
+__all__ = ['AuthPageLocators', 'MainPageLocators', 'OrderPageLocators']
