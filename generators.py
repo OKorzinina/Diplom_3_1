255eee8d99d188ce7793ed76b0cc99ced031a26f
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-29 20:58:54 +0100

    Update generators.py

--- generators.py
+++ generators.py
@@ -1,1 +1,22 @@
+from faker import Faker
 
+
+fake = Faker('ru_RU')
+
+
+class UserGenerator:
+    @staticmethod
+    def generate_user():
+        return {
+            'name': fake.first_name(),
+            'email': fake.email(),
+            'password': fake.password(length=10)
+        }
+    
+    @staticmethod
+    def generate_invalid_email():
+        return "invalid-email"
+    
+    @staticmethod
+    def generate_short_password():
+        return "123"
