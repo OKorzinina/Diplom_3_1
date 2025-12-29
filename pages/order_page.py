2938fcb662fc38d17c6faf303552f0c8c0822133
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-29 20:42:02 +0100

    Update order_page.py

--- pages/order_page.py
+++ pages/order_page.py
@@ -1,1 +1,22 @@
+import allure
+from .base_page import BasePage
+from locators.order_page_locators import OrderPageLocators
+from urls import Urls
 
+
+class OrderPage(BasePage):
+    
+    @allure.step("Открыть ленту заказов")
+    def open(self):
+        self.driver.get(Urls.ORDER_FEED_URL)
+        self.wait_for_visible(OrderPageLocators.ORDER_FEED_TITLE)
+    
+    @allure.step("Получить количество 'Выполнено за всё время'")
+    def get_total_orders_count(self):
+        count_text = self.get_text(OrderPageLocators.TOTAL_ORDERS)
+        return int(count_text.replace(',', ''))
+    
+    @allure.step("Получить количество 'Выполнено за сегодня'")
+    def get_today_orders_count(self):
+        count_text = self.get_text(OrderPageLocators.TODAY_ORDERS)
+        return int(count_text.replace(',', ''))
