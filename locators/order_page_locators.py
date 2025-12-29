b0ccbacd91cfeaeade9727f745e39736b05b5a19
OKorzinina <107882650+OKorzinina@users.noreply.github.com>
2025-12-29 20:49:09 +0100

    Update order_page_locators.py

--- locators/order_page_locators.py
+++ locators/order_page_locators.py
@@ -1,1 +1,8 @@
+from selenium.webdriver.common.by import By
 
+
+class OrderPageLocators:
+    ORDER_FEED_TITLE = (By.XPATH, "//h1[text()='Лента заказов']")
+    TOTAL_ORDERS = (By.XPATH, "//p[text()='Выполнено за всё время:']/following-sibling::p")
+    TODAY_ORDERS = (By.XPATH, "//p[text()='Выполнено за сегодня:']/following-sibling::p")
+    IN_PROGRESS_SECTION = (By.XPATH, "//div[contains(@class, 'OrderFeed_inProgress__')]")
